ba2766c516d89e583361602a0849389842c48b81
Gerhard Pillay <gerhardpillay.gp@gmail.com>
2022-12-12 15:03:44 +0200

    Fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 # Creates an ssh config file to bypass passphrase
 file_line { 'Declare_identity_file':
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/schiool',
+  line    => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn_off_passwd_auth':
